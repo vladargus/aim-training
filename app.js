03a91e4b34f56cf76c5e325dddd167d6c9226b68
Vladislav Yamaliev <vlad.argus@gmail.com>
2023-10-22 16:04:57 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEl = document.querySelector('#time')
+const board = document.querySelector('#board')
+const colors = [
+  '#2ff8a0',
+  '#68ff6b',
+  '#ffd319',
+  '#ff901f',
+  '#ff6b62',
+  '#ff2976',
+  '#01b9ff',
+  '#00fff9',
+]
+let time = 0
+let score = 0
+
+startBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('time-btn')) {
+    time = parseInt(event.target.getAttribute('data-time'))
+    screens[1].classList.add('up')
+    startGame()
+  }
+})
+
+board.addEventListener('click', (event) => {
+  if (event.target.classList.contains('circle')) {
+    score++
+    event.target.remove()
+    createRandomCircle()
+  }
+})
+
+function startGame() {
+  setInterval(decreaseTime, 1000)
+  createRandomCircle()
+  setTime(time)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
+  }
+}
+
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+  timeEl.parentNode.classList.add('hide')
+  board.innerHTML = `<h1>Cчет: <span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+  const circle = document.createElement('div')
+  const size = getRandomNumber(10, 60)
+  const { width, height } = board.getBoundingClientRect()
+  const x = getRandomNumber(0, width - (size + size / 4))
+  const y = getRandomNumber(0, height - (size + size / 4))
+  const color = getRandomColor()
+
+  circle.classList.add('circle')
+  circle.style.width = `${size}px`
+  circle.style.height = `${size}px`
+  circle.style.top = `${y}px`
+  circle.style.left = `${x}px`
+  circle.style.backgroundColor = `${color}`
+  circle.style.boxShadow = `0 0 2px ${color}, 0 0 ${size / 4}px ${color}`
+
+  board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
